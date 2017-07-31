911409e776676e93f32ddbbe524ebf640bc54998
Demo User <demouser@MacBook-Pro.local>
2017-07-31 09:51:25 -0700

    update

--- herd/main.py
+++ herd/main.py
@@ -15,11 +15,42 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+import json
+import random
+import urllib2
+import logging
 
-class MainHandler(webapp2.RequestHandler):
+
+ class Login(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/main')
+            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                nickname, logout_url)
+        else:
+            login_url = users.create_login_url('/map')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+
+        self.response.write(
+            '<html><body>{}</body></html>'.format(greeting))
+
+class AdminPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            if users.is_current_user_admin():
+                self.response.write('You are an administrator.')
+            else:
+                self.response.write('You are not an administrator.')
+        else:
+            self.response.write('You are not logged in.')
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+('/', Login),
+
 ], debug=True)
--- main.py
+++ main.py
@@ -5,3 +5,37 @@ import json
 import random
 import urllib2
 import logging
+
+
+ class Login(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/main')
+            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                nickname, logout_url)
+        else:
+            login_url = users.create_login_url('/map')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+
+        self.response.write(
+            '<html><body>{}</body></html>'.format(greeting))
+
+class AdminPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            if users.is_current_user_admin():
+                self.response.write('You are an administrator.')
+            else:
+                self.response.write('You are not an administrator.')
+        else:
+            self.response.write('You are not logged in.')
+
+
+
+app = webapp2.WSGIApplication([
+('/', Login),
+
+], debug=True)
