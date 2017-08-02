6956c7fb95ee46695d7d4d42b8fa4dda67c8487a
Demo User <demouser@MacBook-Pro.local>
2017-08-01 17:27:34 -0700

    dfgfdg

--- herd/main.py
+++ herd/main.py
@@ -101,16 +101,16 @@ class Store(webapp2.RequestHandler):
             logging.info ('user is: ')
             logging.info (UserID)
 
-            k = Location.query(Location.UserID==user.user_id()).get()
+            exist = Location.query(Location.UserID==user.user_id()).get()
             # Let's first check if this user already exists in the database
             # If they do, get that entry (its key) and modify it
-            if k:
-                instance = k
-                instance.lat = lat
-                instance.lng = lng
+            if exist:
+                update = exist
+                update.lat = lat
+                update.lng = lng
             else:
-                instance = Location(UserID=user.user_id(), lat=lat, lng=lng)
-            instance.put()
+                update = Location(UserID=user.user_id(), lat=lat, lng=lng)
+            update.put()
             logging.info('location store')
 
 
